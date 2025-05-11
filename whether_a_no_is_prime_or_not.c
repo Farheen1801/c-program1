193c7b9c1057bf3b7c0d7ed6d40e02df5a05c635
Farheen1801 <waheedashafi12345@gmail.com>
2025-05-11 12:12:27 +0530

    Add files via upload

---
+++ whether_a_no_is_prime_or_not.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    int n , prime=1;
+    printf("enter a number");
+    scanf("%d" , &n);
+    if (n==0||n==1){
+        printf("%d is not a prime no" , n);
+    }
+
+    for(int i=2;i<n;i++){
+     if (n%i==0 &&n!=2){
+        prime=0;
+        break;
+     }
+    else {
+            prime = 1; 
+        }
+    }
+    
+    if (prime==0){
+        printf("%d is not a prime no" , n);
+    }
+     else if (prime ==1){
+        printf("%d is a prime no" , n);
+     }
+    return 0;
+}
